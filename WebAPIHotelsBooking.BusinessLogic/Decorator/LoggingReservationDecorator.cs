0f7778834222e993ebe407b3bc289df0768ac224
VolodymyrBabushkin <vladimir.2017.35@gmail.com>
2024-11-12 19:12:07 +0200

    Implemented Decorator pattern

---
+++ WebAPIHotelsBooking.BusinessLogic/Decorator/LoggingReservationDecorator.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+using WebAPIHotelsBooking.BusinessLogic.Contracts;
+using WebAPIHotelsBooking.BusinessLogic.Dtos;
+
+namespace WebAPIHotelsBooking.BusinessLogic.Decorator
+{
+    public class LoggingReservationDecorator : ReservationDecorator
+    {
+        private readonly ILogger _logger;
+
+        public LoggingReservationDecorator(IReservation reservation, ILogger logger) : base(reservation)
+        {
+            _logger = logger;
+        }
+
+        public override void Create(ReservationDto reservation, IReservationService service)
+        {
+            _logger.LogInformation($"[LOG]: Reservaton of room with id={reservation.Id} is being processed.");
+            base.Create(reservation, service);
+            _logger.LogInformation($"[LOG]: Reservaton of room with id={reservation.Id} has been processed.");
+        }
+    }
+}
